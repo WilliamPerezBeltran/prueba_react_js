6f27c9a4b5459866281a2c7991f1e9f8f5b7a68e
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2019-11-29 18:36:33 -0500

    Add structure with sidenav and movie components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Movie from './js/components/Movie';
+import Sidenav from './js/components/Sidenav';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div>
+   <Sidenav/>
+   <Movie/>
+
     </div>
   );
 }
---
+++ src/js/components/Sidenav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+   <div>
+        <div class="sidenav">
+          <a href="#">Place it </a>
+          <a href="#">Reservas</a>
+          <a href="#">Películas</a>
+        </div>
+    </div>
+  );
+}
+
+export default App;
