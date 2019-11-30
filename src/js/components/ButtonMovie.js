afe40b19df7dc2306808748d49256f030162a74c
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2019-11-29 22:16:03 -0500

    add form

--- src/js/components/ButtonMovie.js
+++ src/js/components/ButtonMovie.js
@@ -1,42 +1,37 @@
-
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Modal from 'react-responsive-modal';
- import buttomMovie from "./ButtonMovie.css";
-// import FormNewMovie from "./FormNewMovie.css";
-
+import React from "react";
+import ReactDOM from "react-dom";
+import Modal from "react-responsive-modal";
+import buttomMovie from "./ButtonMovie.css";
+import FormNewMovie from "./FormNewMovie";
 
 class ButtomMovie extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      open: false
     };
   }
-  
-  state = {
-    open: false,
-  };
- 
   onOpenModal = () => {
     this.setState({ open: true });
   };
- 
+
   onCloseModal = () => {
     this.setState({ open: false });
   };
- 
+
   render() {
     const { open } = this.state;
     return (
       <div>
-        <button class="Button-movi-button" onClick={this.onOpenModal}>Open modal</button>
+        <button class="Button-movi-button" onClick={this.onOpenModal}>
+          Open modal
+        </button>
         <Modal open={open} onClose={this.onCloseModal} center>
-          <h2>Simple centered modal</h2>
+          <FormNewMovie/>
         </Modal>
       </div>
     );
   }
 }
- 
+
 export default ButtomMovie;
