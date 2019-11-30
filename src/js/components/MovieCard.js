686c0e57e441f72792d2e206da4f0079c194aad0
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2019-11-29 23:42:55 -0500

    add card first advance

---
+++ src/js/components/MovieCard.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class MovieCard extends Component {
+  render() {
+    return (
+      <div>
+      <p>MovieCard</p>
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
--- src/js/components/MovieList.js
+++ src/js/components/MovieList.js
@@ -1,32 +1,80 @@
 import React, { Component } from "react";
 
+const list = [
+	{
+		id: "a",
+		firstname: "Robin",
+		lastname: "Wieruch",
+		year: 1988
+	},
+	{
+		id: "a",
+		firstname: "Robin",
+		lastname: "Wieruch",
+		year: 1988
+	},
+	{
+		id: "a",
+		firstname: "Robin",
+		lastname: "Wieruch",
+		year: 1988
+	},
+	{
+		id: "a",
+		firstname: "Robin",
+		lastname: "Wieruch",
+		year: 1988
+	},
+	{
+		id: "a",
+		firstname: "Robin",
+		lastname: "Wieruch",
+		year: 1988
+	},
+	{
+		id: "a",
+		firstname: "Robin",
+		lastname: "Wieruch",
+		year: 1988
+	},
+	{
+		id: "a",
+		firstname: "Robin",
+		lastname: "Wieruch",
+		year: 1988
+	},
+	{
+		id: "b",
+		firstname: "Dave",
+		lastname: "Davidds",
+		year: 1990
+	}
+];
+
 class MovieList extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			// open: false
+		};
+	}
+
+	// onOpenModal = () => {
+	//   this.setState({ open: true });
+	// };
 	render() {
 		return (
 			<div>
-			<p>sdfd</p>
-			<p>sdfd</p>
-			<p>sdfd</p>
-			<p>sdfd</p>
-			<p>sdfd</p>
-			<p>sdfd</p>
-			<p>sdfd</p>
-			<p>sdfd</p>
-			<p>sdfd</p>
-			<p>sdfd</p>
-			<p>sdfd</p>
-			<p>sdfd</p>
-			<p>sdfd</p>
-			<p>sdfd</p>
-			<p>sdfd</p>
-			<p>sdfd</p>
-			<p>sdfd</p>
-			<p>sdfd</p>
-			<p>sdfd</p>
-			<p>sdfd</p>
-			<p>sdfd</p>
-			<p>sdfd</p>
-			<p>sdfd</p>
+					{list.map(item => (
+				<div>
+						<div key={item.id}>
+							<div>{item.id}</div>
+							<div>{item.firstname}</div>
+							<div>{item.lastname}</div>
+							<div>{item.year}</div>
+						</div>
+				</div>
+					))}
 			</div>
 		);
 	}
